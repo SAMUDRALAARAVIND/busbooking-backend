432f7e093aea61313459a187a4216a66cf48aec3
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-08-28 21:16:33 +0530

    city api added

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-import "dotenv/config";
+const cityRouter = require("./controllers/city");
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8000;
 const MODE = process.env.NODE_ENV || "production";
+
+app.use("/city", cityRouter);
 
 app.listen(PORT, () => {
   console.log(`App is running at ${PORT} in ${MODE} mode`);
