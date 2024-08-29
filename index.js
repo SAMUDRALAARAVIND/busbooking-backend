1d25bb392d461b2433c6de8e856a1a03c0baa5ef
kapil <kapil.personal9711@gmail.com>
2024-08-29 13:04:42 +0530

    added globalErrorHandler to index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 import "dotenv/config";
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// globalError handler
+app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 8000;
 const MODE = process.env.NODE_ENV || "production";
