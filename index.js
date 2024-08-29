92316b1f64da2e1b1a37eade9659d178ed9f67c1
kapil <kapil.personal9711@gmail.com>
2024-08-29 12:51:52 +0530

    changed es-6 import to commonjs required for dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-import "dotenv/config";
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
