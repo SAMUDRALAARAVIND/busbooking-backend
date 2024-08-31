d456fc0191efa0d3f5ee8d54c9d6b81170e52eca
manishloginn <manishv7714@gmail.com>
2024-08-31 13:46:11 +0530

    small changes

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoConnection = require("./connection/mongoConnection");
 const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
-const { registrationControler, loginControler, otpVerify } = require("./controler/Auth");
 const router = require("./routes/routes");
 
 
@@ -17,6 +16,11 @@ app.use(cookieParser())
 
 const PORT = process.env.PORT || 8000;
 const MODE = process.env.NODE_ENV || "production";
+
+
+//mongodb connection
+mongoConnection()
+
 
 app.use("/city", cityRouter);
 app.use('/', router)
